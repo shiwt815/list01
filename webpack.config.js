d29940de52732a131646e295c2c8c8d1f9856fc4
“shiwt815” <815255683@qq.com>
2021-01-19 16:40:48 +0800

    server   html 安装完成

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,9 @@
 const path = require('path')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin') 
 
 //console.log('__dirname: ', __dirname)// E:\my\作业\vueList\newtodoList
 //console.log('path.resolve: ', path.resolve(__dirname, 'dist'))// E:\my\作业\vueList\newtodoList\dist
-
 module.exports = {
    entry: {
     	app: './src/index.js' // 需要打包的文件入口
@@ -17,9 +16,10 @@ module.exports = {
    }, 
    devServer: {
     port: 5000, // 本地服务器端口号
+    contentBase:'./dist'//设置服务器访问的基本目录
   },
    plugins: [
-    	new CleanWebpackPlugin(), // 默认情况下，此插件将删除 webpack output.path目录中的所有文件，以及每次成功重建后所有未使用的 webpack 资产。
+    	// new CleanWebpackPlugin(), // 默认情况下，此插件将删除 webpack output.path目录中的所有文件，以及每次成功重建后所有未使用的 webpack 资产。
    		new HtmlWebpackPlugin({
 		        // 打包输出HTML
 		        title: '自动生成 HTML',
@@ -30,7 +30,7 @@ module.exports = {
 		        minifyCSS: false // 压缩内联 css
 	        },
 	        filename: 'index.html', // 生成后的文件名
-	        template: 'index.html' // 根据此模版生成 HTML 文件
+	        template: path.join(__dirname, './index.html') // 根据此模版生成 HTML 文件
 	    })
    ],
    module: {
@@ -44,6 +44,5 @@ module.exports = {
 		    }
 	 	]
 	}
-
-
 }
+// console.log('sssssssssss: ',path.join(__dirname, 'index.html'))
