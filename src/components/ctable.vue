04fc73f0fdeadc96fa4ec0e2ef3ca480aa67c52e
“shiwt815” <815255683@qq.com>
2021-02-02 10:41:49 +0800

    组件传参

--- src/components/ctable.vue
+++ src/components/ctable.vue
@@ -1,24 +1,17 @@
 <template>
 	<div>
-		<table>
 			<tr>
-				<th>编号</th>
-				<th>名称</th>
-				<th>时间</th>
-				<th>操作</th>
-			</tr>
-			<tr>
-				<td>0</td>
-				<td>第一条信息</td>
-				<td>05/15</td>
+				<td>{{indexsort}}</td>
+				<td>{{content.nameValue}}</td>
+				<td>{{content.timeValue}}</td>
 				<td><button>操作</button></td>
 			</tr>
-		</table>
 	</div>
 </template>
 <script>
 	export default {
 	  name: 'app',
+  	  props:['content',"index"],
 	 
 	}
 </script>
--- src/components/isComponent.vue
+++ src/components/isComponent.vue
@@ -3,14 +3,22 @@
 		<h1>有组件</h1>
 		<div class="top-box">
 			<span>名称：</span>
-			<input type="text">
+			<input type="text" v-model="nameValue">
 			<span>时间：</span>
-			<input type="text">
+			<input type="text" v-model="timeValue">
 			<button>提交</button>
 			<button>全部删除</button>
 		</div>
 
-		<ctable></ctable>
+		<table>
+			<tr>
+				<th>编号</th>
+				<th>名称</th>
+				<th>时间</th>
+				<th>操作</th>
+			</tr>
+			<ctable v-for="(item1 , index) of list" :key="index":indexsort="index" :content="item1"></ctable>
+		</table>
 	</div>
 </template>
 <script>
@@ -24,22 +32,36 @@
 			return {
 				nameValue:"",
 				timeValue:"",
-				isActive:false,
-				color:"红",
 				list:[
 					{
-						nameValue:"3",
+						nameValue:"1",
 						timeValue:"ffdf"
 					},
 					{
-						nameValue:"4",
+						nameValue:"2",
 						timeValue:"ggggg"
 					},
 					{
-						nameValue:"5",
+						nameValue:"3",
 						timeValue:"jjjjj"
 					}
 				],
+				// isActive:false,
+				// color:"红",
+				// list:[
+				// 	{
+				// 		nameValue:"3",
+				// 		timeValue:"ffdf"
+				// 	},
+				// 	{
+				// 		nameValue:"4",
+				// 		timeValue:"ggggg"
+				// 	},
+				// 	{
+				// 		nameValue:"5",
+				// 		timeValue:"jjjjj"
+				// 	}
+				// ],
 			}
 		},
 		methods:{
